b29276a75c8aeb0869096f3d05c00ad7147a722c
Alexander Borsuk <me@alex.bio>
2023-08-24 00:29:54 +0200

    Print housenumbers

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,81 @@
 use osmpbf::{ElementReader, Element};
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::env;
+use std::{env, io};
 use std::fs;
-use std::io::Write;
-use std::path::Path;
-use fs::File;
+use std::io::{BufWriter, Stdout, Write};
+// use std::path::Path;
+// use fs::File;
 
-const KEYS_PREFIXES: [&str; 9] = ["name:", "int_name", "loc_name", "nat_name", "official_name", "old_name", "reg_name", "short_name", "alt_name"];
-const KEYS: [&str; 3] = ["name", "addr:street", "addr:housename"];
+//const KEYS_PREFIXES: [&str; 9] = ["name:", "int_name", "loc_name", "nat_name", "official_name", "old_name", "reg_name", "short_name", "alt_name"];
+//const KEYS: [&str; 3] = ["name", "addr:street", "addr:housename"];
 
 type ValuesSet = HashSet<String> ;
 type KeysMap = HashMap<String, ValuesSet>;
 
-fn save_tags<'a, Iter: std::iter::Iterator<Item = (&'a str, &'a str)>>(iter: Iter, keys_map: &mut KeysMap) {
+fn save_tags<'a, Iter: Iterator<Item = (&'a str, &'a str)>>(iter: Iter, handle: &mut BufWriter<Stdout>) {
     for (key, value) in iter {
-        if KEYS.contains(&key) || KEYS_PREFIXES.iter().any(|&s| key.starts_with(s)) {
-            match keys_map.get_mut(key) {
-                Some(set) => {
-                    set.insert(value.to_string());
-                    ()
-                },
-                None => {
-                    keys_map.insert(key.to_string(), HashSet::from([value.to_string()]));
-                    ()
-                }
-            }
+        //if KEYS.contains(&key) || KEYS_PREFIXES.iter().any(|&s| key.starts_with(s)) {
+        if key == "addr:housenumber" {
+            writeln!(handle, "{value}").expect("Write to stdout failed");
+            // match keys_map.get_mut(key) {
+            //     Some(set) => {
+            //         set.insert(value.to_string());
+            //         ()
+            //     },
+            //     None => {
+            //         keys_map.insert(key.to_string(), HashSet::from([value.to_string()]));
+            //         ()
+            //     }
+            // }
         }
     }
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Usage: {} <path to osm.pbf file>", args[0]);
+        println!("Usage: {} <path to osm.pbf file> [optional output file]", args[0]);
         return Ok(());
     }
 
-    let file_path = &args[1];
-    let reader = ElementReader::from_path(file_path).unwrap();
+    let use_stdout = args.len() < 3;
+    let out_file = if !use_stdout { &args[2] } else { "" };
+
+    let osm_pbf_path = &args[1];
+    let reader = ElementReader::from_path(osm_pbf_path).unwrap();
 
     let mut keys: KeysMap = HashMap::new();
 
-    // Increment the counter by one for each way.
+    let stdout = io::stdout(); // get the global stdout entity
+    let mut handle = io::BufWriter::new(stdout); // optional: wrap that handle in a buffer
+
     reader.for_each(|element| {
         match element {
-            Element::Node(e) => save_tags(e.tags(), &mut keys),
-            Element::DenseNode(e) => save_tags(e.tags(), &mut keys),
-            Element::Way(e) => save_tags(e.tags(), &mut keys),
-            Element::Relation(e) => save_tags(e.tags(), &mut keys),
+            Element::Node(e) => save_tags(e.tags(), &mut handle),
+            Element::DenseNode(e) => save_tags(e.tags(), &mut handle),
+            Element::Way(e) => save_tags(e.tags(), &mut handle),
+            Element::Relation(e) => save_tags(e.tags(), &mut handle),
         }
     }).unwrap();
 
     // Write collected data to files.
-    let dir = Path::new("./out");
-    fs::create_dir_all(dir)?;
-
-    for (key, set) in keys.iter() {
-        let file = dir.join(key);
-        let writer = File::options().create(true).write(true).open(&file);
-        if writer.is_err() {
-            println!("Error creating {} : {}", file.display(), writer.unwrap_err());
-            continue;
-        }
-        let mut writer = writer.unwrap();
-        println!("Writing {} values to {}", set.len(), file.display());
-        for val in set.iter() {
-            writeln!(writer, "{val}").expect("Write failed");
-        }
-    }
+    // let dir = Path::new("./out");
+    // fs::create_dir_all(dir)?;
+    //
+    // for (key, set) in keys.iter() {
+    //     let file = dir.join(key);
+    //     let writer = File::options().create(true).write(true).open(&file);
+    //     if writer.is_err() {
+    //         println!("Error creating {} : {}", file.display(), writer.unwrap_err());
+    //         continue;
+    //     }
+    //     let mut writer = writer.unwrap();
+    //     println!("Writing {} values to {}", set.len(), file.display());
+    //     for val in set.iter() {
+    //         writeln!(writer, "{val}").expect("Write failed");
+    //     }
+    // }
 
     Ok(())
 }
